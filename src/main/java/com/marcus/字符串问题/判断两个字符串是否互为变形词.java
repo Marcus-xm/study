10db7c8f74057c74ac9170026909c61271c78c80
方纪正 <844353798@qq.com>
2022-06-30 10:21:32 +0800

    test

--- src/main/java/com/marcus/字符串问题/判断两个字符串是否互为变形词.java
+++ src/main/java/com/marcus/字符串问题/判断两个字符串是否互为变形词.java
@@ -4,4 +4,11 @@ package com.marcus.字符串问题;
  * Created by marcus on 2022/6/29.
  */
 public class 判断两个字符串是否互为变形词 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean str(String str1,String str2){
+        return true;
+    }
 }
