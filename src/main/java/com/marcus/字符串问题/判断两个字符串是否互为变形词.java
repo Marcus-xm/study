b35c51f5e8718ed5e3e10e3513e7d8b4297ef763
marcus <58065199+Marcus-xm@users.noreply.github.com>
2022-06-30 10:24:24 +0800

    test

--- src/main/java/com/marcus/字符串问题/判断两个字符串是否互为变形词.java
+++ src/main/java/com/marcus/字符串问题/判断两个字符串是否互为变形词.java
@@ -5,7 +5,7 @@ package com.marcus.字符串问题;
  */
 public class 判断两个字符串是否互为变形词 {
     public static void main(String[] args) {
-
+        str("abc","adc");
     }
 
     public boolean str(String str1,String str2){
